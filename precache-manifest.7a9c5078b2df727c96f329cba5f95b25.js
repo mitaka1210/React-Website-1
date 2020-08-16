eea021820c8784b2eb06fefd4bf2dd5e2db35954
Dimitar <dimitar_dimitrov12@mail.bg>
2020-08-16 18:24:17 +0300

    Updates

---
+++ precache-manifest.7a9c5078b2df727c96f329cba5f95b25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49b0bbcd515809d79fa076d0a344ea5d",
+    "url": "/React-Website-1/index.html"
+  },
+  {
+    "revision": "a7fcbd1ed21d330381e5",
+    "url": "/React-Website-1/static/css/main.8ac133d1.chunk.css"
+  },
+  {
+    "revision": "3c65eb0aaa94194eb0bf",
+    "url": "/React-Website-1/static/js/2.805b82d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/React-Website-1/static/js/2.805b82d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a7fcbd1ed21d330381e5",
+    "url": "/React-Website-1/static/js/main.0e8e9fe0.chunk.js"
+  },
+  {
+    "revision": "b71dee491f6225727d30",
+    "url": "/React-Website-1/static/js/runtime-main.8f745e1a.js"
+  }
+]);
